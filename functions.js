e6e48e2a7934d3e7973845a41c68066f6d544a2a
temperance01 <rralphsarz@gmail.com>
2023-05-25 01:59:59 +0800

    commit

--- functions.js
+++ functions.js
@@ -1,11 +1,22 @@
 function togglePasswordVisibility() {
-    const passwordInput = document.getElementById('password');
-    const passwordCheckbox = document.getElementById('passwordCheckbox');
-
-    if (passwordCheckbox.checked) {
-        passwordInput.type = 'text';
+    const loginPasswordInput = document.getElementById('password');
+    const loginPasswordCheckbox = document.getElementById('loginPasswordCheckbox');
+    const signupPasswordInput = document.getElementById('signupPassword');
+    const signupPasswordCheckbox = document.getElementById('signupPasswordCheckbox');
+  
+    if (loginPasswordCheckbox && loginPasswordCheckbox.checked) {
+      // Show password for login form
+      loginPasswordInput.type = 'text';
     } else {
-        passwordInput.type = 'password';
+      // Hide password for login form
+      loginPasswordInput.type = 'password';
     }
-}
-
+  
+    if (signupPasswordCheckbox && signupPasswordCheckbox.checked) {
+      // Show password for signup form
+      signupPasswordInput.type = 'text';
+    } else {
+      // Hide password for signup form
+      signupPasswordInput.type = 'password';
+    }
+  }
